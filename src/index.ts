52a611878405123605031f09b2ca9ce72351f9de
shu-suzuki-1124 <103195099+shu-suzuki-1124@users.noreply.github.com>
2022-07-23 22:49:45 +0900

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+class ProjectInput {
+  templeteElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templeteElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templeteElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
